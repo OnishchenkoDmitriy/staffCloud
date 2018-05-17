5525ad3da50e71c07c2c11b344764cf0932c1982
Dmytro Onyshchenko <Dmytro_Onyshchenko@epam.com>
2018-05-17 22:53:37 +0300

    Created project structure

---
+++ be/src/main/java/org/staff/cloud/staff_cloud/StaffCloudApplication.java
@@ -0,0 +1,12 @@
+package org.staff.cloud.staff_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffCloudApplication.class, args);
+    }
+}
